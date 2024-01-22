72d71653b258da5d6184894d9a6378254be825c9
Faithful <faithfuleromosele001@gmail.com>
2024-01-22 15:02:14 +0100

    set up the socket

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,22 @@ import express from "express";
 import cors from "cors";
 import http from "http";
 import { Server } from "socket.io";
+import { config } from "dotenv";
+config();
 
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+const corsOptions: object = {
+	origin: ["http://localhost:3000"],
+	methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsOptions));
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-	cors: {
-		origin: ["http://localhost:3000"],
-		methods: ["GET", "POST", "PUT", "DELETE"],
-	},
+	cors: corsOptions,
 });
 
 io.on("connection", (socket) => {
@@ -25,6 +28,8 @@ io.on("connection", (socket) => {
 	});
 });
 
-server.listen(3001, () => {
-	console.log("Server running on port 3001");
+const PORT = process.env.PORT;
+
+server.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
 });
